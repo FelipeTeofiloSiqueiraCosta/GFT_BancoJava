f116c3c5b3964948f8f630a221382041627f7490
FelipeTeofiloSiqueiraCosta <felipeteofilosiqueiracosta@gmail.com>
2022-04-28 07:50:28 -0300

    Update Main.java

--- lab-banco-digital-oo-master/src/Main.java
+++ lab-banco-digital-oo-master/src/Main.java
@@ -3,13 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		venilton.setNome("Felipe Costa");
 		
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		cc.depositar(1000);
+		cc.transferir(200, poupanca);
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
